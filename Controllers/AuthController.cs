e1b52cfb08925afca60dad809174c4da0cbc7532
Jhonatan Pacheco <jhonatan.tec@gmail.com>
2025-06-24 20:59:30 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fiap_arquitetura_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IConfiguration configuration) : ControllerBase
+    {
+    }
+}
---
+++ Controllers/SecureController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fiap_arquitetura_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecureController : ControllerBase
+    {
+        [Authorize(Policy = "Admin")]
+        [HttpGet("admin")]
+        public IActionResult GetAdminOnly()
+        {
+            return Ok("Acesso permitido para ADMIN");
+        }
+
+        [Authorize(Policy = "Usuario")]
+        [HttpGet("usuario")]
+        public IActionResult GetUsuarioOnly()
+        {
+            return Ok("Acesso permitido para USUÁRIO");
+
+        }         
+
+    }
+}
